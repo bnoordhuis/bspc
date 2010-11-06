787848fc998d1f339b7d6979d5a0575652407c05
Ben Noordhuis <info@bnoordhuis.nl>
2010-11-06 22:51:25 +0100

    Fix include paths.

--- deps/qcommon/qcommon.h
+++ deps/qcommon/qcommon.h
@@ -23,7 +23,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #ifndef _QCOMMON_H_
 #define _QCOMMON_H_
 
-#include "../qcommon/cm_public.h"
+#include "cm_public.h"
 
 //Ignore __attribute__ on non-gcc platforms
 #ifndef __GNUC__

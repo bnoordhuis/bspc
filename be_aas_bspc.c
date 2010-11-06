95355960c480cbcc6554e35cce3df66e359e9db3
Ben Noordhuis <info@bnoordhuis.nl>
2010-11-06 17:28:45 +0100

    Remove Com_Memset and Com_Memcpy, aliased as memset and memcpy respectively in deps/qcommon/q_shared.h

--- be_aas_bspc.c
+++ be_aas_bspc.c
@@ -219,24 +219,6 @@ int COM_Compress( char *data_p ) {
 }
 //===========================================================================
 //
-// Parameter:			-
-// Returns:				-
-// Changes Globals:		-
-//===========================================================================
-void Com_Memset (void* dest, const int val, const size_t count) {
-	memset(dest, val, count);
-}
-//===========================================================================
-//
-// Parameter:			-
-// Returns:				-
-// Changes Globals:		-
-//===========================================================================
-void Com_Memcpy (void* dest, const void* src, const size_t count) {
-	memcpy(dest, src, count);
-}
-//===========================================================================
-//
 // Parameter:				-
 // Returns:					-
 // Changes Globals:		-

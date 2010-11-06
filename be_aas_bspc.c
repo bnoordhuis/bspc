ae6fea33d110dada04e922d0340e4a9e11ee2e42
Ben Noordhuis <info@bnoordhuis.nl>
2010-11-06 17:20:09 +0100

    Fix include path.

--- be_aas_bspc.c
+++ be_aas_bspc.c
@@ -21,8 +21,8 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
 #include "qcommon/q_shared.h"
-#include "bspc/l_log.h"
-#include "bspc/l_qfiles.h"
+#include "l_log.h"
+#include "l_qfiles.h"
 #include "botlib/l_memory.h"
 #include "botlib/l_script.h"
 #include "botlib/l_precomp.h"

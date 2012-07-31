fb9526e89c296165356c5494b66588c3fe4142fe
Ben Noordhuis <info@bnoordhuis.nl>
2012-07-31 02:21:03 +0200

    Fix implicit function declaration warnings.

--- be_aas_bspc.h
+++ be_aas_bspc.h
@@ -21,3 +21,4 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
 void AAS_CalcReachAndClusters(struct quakefile_s *qf);
+void AAS_InitBotImport(void);
--- bspc.c
+++ bspc.c
@@ -20,6 +20,8 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 ===========================================================================
 */
 
+#define AASINTERN /* make more botlib function prototypes visible */
+
 #if defined(WIN32) || defined(_WIN32)
 #include <direct.h>
 #include <windows.h>
